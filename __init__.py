f278b5d14052661c6f41209bfcf05af06be304e6
Tryamid <bsantanu381@gmail.com>
2020-02-17 12:25:10 +0530

    Collect commands from other files and append them into the bot.

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from   discord.ext.commands import *
+from   os                   import getenv
+from   itertools            import chain
+
+import basic
+
+__commandcats__ = (basic) # list of command categories.
+
+bot = Bot('?', case_insensitive= True)
+
+for cmd in chain(*map(lambda cc: cc.__commands__, __commandcats__)):
+    bot.add_command(cmd)
+
+# authorize by the token in the environment variables.
+bot.run(getenv('DISCORD_ACCESS_TOKEN'))
