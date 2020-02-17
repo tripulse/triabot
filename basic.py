b483a2099ef669f261f6dd4e03e0429d42d674de
Tryamid <bsantanu381@gmail.com>
2020-02-17 13:08:07 +0530

    Warped all sub-classed commands into async functions with a decorator.

--- basic.py
+++ basic.py
@@ -7,43 +7,35 @@ from numpy.fft import rfft
 from functools import reduce
 from random import choice
 
-# class BasicCommand(Command):
-#     def __init__(self):
-#         Command.__init__(self, name= type(self).__name__, 
-#                                func= self._run, 
-#                                help= self.__doc__)
 
-@command
-async def ping(BasicCommand):
+@command()
+async def ping(ctx: Context):
     """Calculates the delta between message sent and message recieved.
     It's a mixture of multiple delays of arbitary time."""
 
-    async def _run(cls, ctx: Context):
-        tdelta = datetime.utcnow() - ctx.message.created_at
-        await ctx.send(f'\u0394`{tdelta}`')
+    tdelta = datetime.utcnow() - ctx.message.created_at
+    await ctx.send(f'\u0394`{tdelta}`')
 
-# class computerfft(BasicCommand):
-#     """Computes 1D RFFT (real-valued fast-fourier transform) over arbitary
-#     numeric values separted with spaces."""
+@command
+async def computerfft(ctx: Context, *nums):
+    """Computes 1D RFFT (real-valued fast-fourier transform) over arbitary
+    numeric values separted with spaces."""
+    
+    invals = tuple(map(lambda n: float(n), nums))
+    outvals = rfft(invals).tolist()
 
-#     @classmethod
-#     async def _run(cls, ctx: Context, *nums):
-#         invals = tuple(map(lambda n: float(n), nums))
-#         outvals = rfft(invals).tolist()
+    await ctx.send(' '.join(map(
+        lambda n: f'{str(n.real)}+i{str(n.imag)}',
+    outvals)))
 
-#         await ctx.send(' '.join(map(
-#             lambda n: f'{str(n.real)}+i{str(n.imag)}',
-#         outvals)))
+async def skwiggle(ctx: Context, *string_slices):
+    """Idiotizes strings by randomizing the case of each letter.
+    This method is mostly used to denote sentences/thoughts that are
+    not in favour of the author."""
 
-# class skwiggle(BasicCommand):
-#     """Idiotizes strings by randomizing the case of each letter.
-#     This method is mostly used to denote sentences/thoughts that are
-#     not in favour of the author."""
+    instr = ' '.join(string_slices)
+    await ctx.send(''.join([choice((c.upper, c.lower))() for c in instr]))
 
-#     @classmethod
-#     async def _run(cls, ctx: Context, *string_slices):
-#         instr = ' '.join(string_slices)
-#         await ctx.send(''.join([choice((c.upper, c.lower))() for c in instr]))
 
-"""List of commands available in this single module."""
-__commands__ = (ping)
+"""Collection of commands exported from this module."""
+__commands__ = (ping,)
