62e1d08bac2e9a51e7ae1846cfe1a6080df51318
tripulse <bsantanu381@gmail.com>
2020-04-28 08:56:48 +0530

    * Change the bot-prefix to: '_' * Remove arg from a Cog registration.

--- __main__.py
+++ __main__.py
@@ -1,12 +1,12 @@
 from   discord.ext.commands import Bot
 from   os                   import getenv
 
-bot = Bot('?', case_insensitive= True)
+bot = Bot('_', case_insensitive=True)
 
 # all Cog registrations go here:
 from cogs.basic import Basic
 
-bot.add_cog(Basic(bot))
+bot.add_cog(Basic())
 
 # authorize by the token in the environment variables.
 bot.run(getenv('DISCORD_ACCESS_TOKEN'))
