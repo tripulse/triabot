937f773e896121f7dad78910d135de4c60854dea
Francesco Zucca <francescozucca30@gmail.com>
2020-05-18 16:01:43 +0200

    Added hunger games

--- __main__.py
+++ __main__.py
@@ -5,8 +5,10 @@ bot = Bot('_', case_insensitive=True)
 
 # all Cog registrations go here:
 from cogs.basic import Basic
+from cogs.games import Games
 
 bot.add_cog(Basic())
+bot.add_cog(Games())
 
 # authorize by the token in the environment variables.
 bot.run(getenv('DISCORD_ACCESS_TOKEN'))
