e4be82f48ff65a2cf2be16b941d65320ecf2097b
tripulse <bsantanu381@gmail.com>
2020-05-20 16:32:53 +0530

    * Handle some exceptions if cannot print them right to the stderr. * DISCORD_ACCESS_TOKEN is changed to DAPI_ACCESS_TOKEN environ. * Exclude modules inside the comamnd module folder. Might be changed later on.

--- __main__.py
+++ __main__.py
@@ -1,20 +1,40 @@
-from discord.ext.commands import Bot, check
 from os                   import getenv, listdir
-from os.path              import join, sep
 from asyncio              import run
+from sys                  import stderr
+
+from discord.ext.commands import (
+    Bot, check,
+    NoPrivateMessage,
+    MissingPermissions,
+    BotMissingPermissions,
+)
 
 # some constants to work with, these are got from environ vars.
 COMMODULES = getenv('COMMAND_MODULE_DIR', 'cogs')
+MODEXCLUDES = ['utils.py']  # exclude some files from coglist.
 BOTHOST_ID = getenv('BOT_HOST_ID')
 
 bot = Bot('_', case_insensitive=True)
+
+# Handles some exceptions if handler not defined print
+# the traceback to the sys.stderr for debugging later on.
+@bot.event
+async def on_command_error(ctx, exc):
+    if isinstance(exc, NoPrivateMessage):
+        await ctx.send("This command cannot be run outside the"
+                       "context of a guild (eg. DMs, Group DMS).")
+    elif isinstance(exc, (MissingPermissions,
+                          BotMissingPermissions)):
+        await ctx.send(str(exc))
+    else:  # if not handler satisfies the exception.
+        stderr.write(str(exc))
 
 @bot.command()
 @check(lambda ctx: str(ctx.author.id) == BOTHOST_ID)
 async def load(*_):
     for mod in listdir(COMMODULES):
         modname, _, ext = mod.partition('.')        
-        if ext == 'py':
+        if ext == 'py' and not mod in MODEXCLUDES:
             # choose whether to reload or just load based on
             # whether the module is already loaded or not.
             (bot.reload_extension if modname in bot.cogs.keys()
@@ -23,6 +43,6 @@ async def load(*_):
 try:
     run(load())  # load all the command modules,
                  # at the startup.
-    bot.run(getenv('DISCORD_ACCESS_TOKEN'))
+    bot.run(getenv('DAPI_ACCESS_TOKEN'))
 except KeyboardInterrupt:
     run(bot.logout())
