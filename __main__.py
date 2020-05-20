4f1ab7059061b83641d4afe86d3838b3f877898d
tripulse <bsantanu381@gmail.com>
2020-05-20 13:52:22 +0530

    * A bot reloader which reloads all the modules with a command. * Logout from Discord API if KeyboardInterrupt occured.

--- __main__.py
+++ __main__.py
@@ -1,36 +1,28 @@
-from   discord.ext.commands import Bot, check
-from   discord              import Game, Status
-from   os                   import getenv, listdir
-from   checks               import is_bot_owner
+from discord.ext.commands import Bot, check
+from os                   import getenv, listdir
+from os.path              import join, sep
+from asyncio              import run
+
+# some constants to work with, these are got from environ vars.
+COMMODULES = getenv('COMMAND_MODULE_DIR', 'cogs')
+BOTHOST_ID = getenv('BOT_HOST_ID')
 
 bot = Bot('_', case_insensitive=True)
 
-# Cogs loading
-for cog in listdir("cogs"):
-    if ".py" == cog[-3:]:
-        bot.load_extension("cogs."+cog[:-3])
-
-# Cogs reload command
 @bot.command()
-@check(is_bot_owner)
-async def reload(ctx):
+@check(lambda ctx: str(ctx.author.id) == BOTHOST_ID)
+async def load(*_):
+    for mod in listdir(COMMODULES):
+        modname, _, ext = mod.partition('.')        
+        if ext == 'py':
+            # choose whether to reload or just load based on
+            # whether the module is already loaded or not.
+            (bot.reload_extension if modname in bot.cogs.keys()
+             else bot.load_extension)(f'{COMMODULES}.{modname}')
 
-    await bot.change_presence(status=Status.idle)
-
-    for cog in listdir("cogs"):
-        if ".py" == cog[-3:]:
-            if cog[:-3] in bot.cogs.keys():
-                bot.reload_extension("cogs."+cog[:-3])
-            else:
-                bot.load_extension("cogs."+cog[:-3])
-    
-    await bot.change_presence(status=Status.online)
-
-# Stop command
-@bot.command()
-@check(is_bot_owner)
-async def stop(ctx):
-    await bot.logout()
-
-# authorize by the token in the environment variables.
-bot.run(getenv('DISCORD_ACCESS_TOKEN'))
+try:
+    run(load())  # load all the command modules,
+                 # at the startup.
+    bot.run(getenv('DISCORD_ACCESS_TOKEN'))
+except KeyboardInterrupt:
+    run(bot.logout())
