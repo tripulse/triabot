8bdd866d53e79e0720db7128a124db67fed15c1f
Santanu <bsantanu381@gmail.com>
2020-06-07 22:13:41 +0530

    Code improvement plus bugfixes.
    purge command can now remove messages for a given amount without mentionting a user.
    And now leaves amount of messages that were deleted.
    
    rndspc, rndcap were didn't capture text perfectly, now does it perefectly.
    
    rndspc is has dropped the continiously increasing logarithmic spacing, now takes a user defined amount.
    
    ping is replaced with test, which displays the websocket delay.

--- ops/basic.py
+++ ops/basic.py
@@ -14,37 +14,40 @@ from ._utils              import (
 
 class Basic(Cog):
     @command()
-    async def ping(self, ctx):
-        "Computes the delay of the WebSocket (unit: miliseconds)."
-        await ctx.send(ctx, f'{round(ctx.bot.latency*1000, 3)} ms')
+    async def test(self, ctx):
+        """This command is verify that the bot's command parsing algorithm is working
+        fine, it returns the WebSocket delay in miliseconds"""
+
+        await ctx.send(f"{round(ctx.bot.latency * 1000, 2)}ms")
 
     @command()
     async def rand(self, ctx, min: float, max: float):
-        "Generate a pseudo-random number between a given bound (min..max]."
+        """Given a decimal bound as (min..max] it uses uniform distribution to pick
+        a pseudo-random number from that range"""
+
         await ctx.send(uniform(min, max))
 
     @command()
-    async def pick(self, ctx, *options):
-        "Pick an option from a provided list of options (separted by spaces) randomly."
+    async def rndpick(self, ctx, *options):
+        """Given a arbitrary number of options to choose from, this chooses an option
+        from list of those pseudo-randomly"""
+
         await ctx.send(choice(options))
 
     @command()
-    async def rndspc(self, ctx, *frags):
-        """Joins single-letters with spaces with random spaces, using a little-complex alogirthms."""
+    async def space(self, ctx, spaces: int, *, text: str):
+        """Spaces characters with a defined amount to E M P H A S I Z E a statement, these
+        are mostly used to exaggerate some statement (eg. A  E  S  T  H  E  T  I  C  S)"""
 
-        await ctx.send(''.join(
-            c + ' ' * int(log1p(i)) for i,c in
-                enumerate(chain(*frags))))
+        await ctx.send(''.join(c + ' ' * spaces for c in text))
 
     @command()
-    async def rndcap(self, ctx, *frags):
-        """Does alternate capitalization of text, this is mostly
-        used to say a wrong statement in sarcastic way. See this:
+    async def rndcap(self, ctx, *, text: str):
+        """Randomly capitalizes each and every English word of a text to emphasize the
+        text as if it was wrong in a ironic way. For information:
         https://english.stackexchange.com/q/533036"""
 
-        await ctx.send(''.join(
-            choice([c.upper, c.lower])() for c in
-                ' '.join(frags)))
+        await ctx.send(''.join(choice([c.upper, c.lower])() for c in text))
 
 class Utils(Cog):
     """Contains utilities for Discord moderation, though these features
@@ -54,17 +57,17 @@ class Utils(Cog):
     @command()
     @author_has_guild_permissions(manage_messages=True)
     @bot_has_guild_permissions(manage_messages=True, read_message_history=True)
-    async def purge(self, ctx, target: User, num: int=10):
-        """Bulk delete messages of a certain amount posted
-        by a targetted Discord user."""
+    async def purge(self, ctx, num: int=10, target: User=None):
+        """Bulk delete messages of a certain amount posted by a targetted Discord user,
+        if not provided a user it just deletes all the messages which it encounters"""
 
         msgs_deleted = 0
         async for msg in ctx.history(limit=None):
-            if not msgs_deleted < num: break
-            if msg.author == target:
+            if not msgs_deleted < num:
+                await ctx.send(f"Purged {num} messages in {ctx.channel}.")
+                break
+            if msg.author == target or target is None:
                 await msg.delete()
                 msgs_deleted+= 1
-
-        await ctx.send(f"Purged {msgs_deleted} messages sent by @{target.tag}.")
-
+        
 __cogexport__ = [Basic, Utils]
