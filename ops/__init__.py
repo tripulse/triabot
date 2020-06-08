4ea5699b9ded289f5a5479369122143a4af2fc04
tripulse <bsantanu381@gmail.com>
2020-06-08 12:38:37 +0530

    Use more characters in a line, don't follow the 80-char limit.

--- ops/__init__.py
+++ ops/__init__.py
@@ -20,18 +20,14 @@ def _getall_cogs():
         module = import_module(f'.{module}', __moddir_name__)
 
         cogs = getattr(module, '__cogexport__', None)
-        return [] if not isinstance(cogs, list) else \
-                filter(lambda cog: issubclass(cog, Cog), cogs)
+        return [] if not isinstance(cogs, list) else filter(lambda cog: issubclass(cog, Cog), cogs)
 
-    return chain(*map(lambda mod:
-                _cogextract(filename.from_str(mod).name),
-                    Path(__moddir_path__).glob('*.py')))
+    return chain(*map(lambda mod: _cogextract(filename.from_str(mod).name), Path(__moddir_path__).glob('*.py')))
 
 async def on_command_error(ctx, exc):
-    """A custom `on_command_error` handler, handles some excpetions
-    thrown in commands and translates into client compatible error
-    messages for the command invoker/developer understand what had
-    happened internally and possible reasons of this."""
+    """A custom `on_command_error` handler, handles some excpetion thrown in commands and
+    translates into client compatible error messages for the command invoker/developer
+    understand what had happened internally and possible reasons of this."""
     
     if isinstance(exc, NoPrivateMessage):
         await ctx.send("This command cannot be run outside the"
