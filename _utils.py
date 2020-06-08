1614599d4b9dfd2a00c1c62e7cb8a6f90a71f0ed
tripulse <bsantanu381@gmail.com>
2020-06-08 12:36:18 +0530

    Use a separate function for handling command description decoration.

--- _utils.py
+++ _utils.py
@@ -5,15 +5,17 @@ class EmbedHelp(HelpCommand):
   """Help-command formatter that uses multiple embeds for each cogs or global level commands.
   It is considered better than the default formatter that multiple codeblocks as pages."""
 
-  async def send_bot_help(self, mappings):
-    "Send all the global and Cog specific commands."
+  async def _prepare_command_desc(self, command) -> str:
+    "Given a command prepare its description."
 
+    # the signature is kept bold and the rest unchanged.
+    return f"**{self.get_command_signature(command)}**\n{command.help}"
+
+  async def send_bot_help(self, mappings):
     for cog, commands in mappings.items():
         await self._send_cog_help(cog, commands)
   
   async def send_cog_help(self, cog):
-    "Send help message for a defined Cog."
-
     await self._send_cog_help(cog, cog.get_commands())
   
   async def _send_cog_help(self, cog, commands):
@@ -23,25 +25,19 @@ class EmbedHelp(HelpCommand):
 
     for command in await self.filter_commands(commands, sort=True):
       helpmsg.add_field(name=command.name, inline=False, value=
-        f"**{self.get_command_signature(command)}**\n{command.help}")
+        self.prepare_help_command(command))
     
     await self.get_destination().send(embed=helpmsg)
 
   async def send_command_help(self, command):
-    "Send help message for a defined command."
-
     await self.get_destination().send(embed=
-      Embed(title=command.name, description=
-      f"**{self.get_command_signature(command)}**\n{command.help}"))
+      Embed(title=command.name, description=self._prepare_command_desc(command)))
 
   async def send_group_help(self, group):
-    "Send help message for a defined group."
-
-    helpmsg = Embed(title=group.name, description=
-      f"**{self.get_command_signature(group)}**\n{group.help}")
+    helpmsg = Embed(title=group.name, description=self._prepare_command_desc(group))
       
     for command in group.commands:
-      helpmsg.add_field(name=command.name, inline=False, value=
-        f"**{self.get_command_signature(command)}**\n{command.help}")
+      helpmsg.add_field(name=command.name, inline=False,
+        value=self._prepare_command_desc(command))
       
     await self.get_destination().send(embed=helpmsg)
